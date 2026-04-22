78eab38418bb48edf81d9bea7bda68dd01dfa65e
Patricia2722 <patriciamaevillariz@gmail.com>
2026-04-22 13:59:54 +0800

    origin txt

--- frontend/src/components/3D_Modeling/3D_Origin.tsx
+++ frontend/src/components/3D_Modeling/3D_Origin.tsx
@@ -132,39 +132,25 @@ const OriginLesson: React.FC<OriginLessonProps> = ({
                 <div className={`instruction-step ${currentIndex === 0 ? "reading-active" : ""}`}>
                   <h3>Use this tool to set the location of origin</h3>
                   <div className="step-header">
-                    <span className="step-number">1</span>
-                    <span className="step-label">Select <strong>Change 3D Part Layout</strong> from the icon menu.</span>
+
+                    <div style={{ marginBottom: "0.8rem" }}>1. Select Change 3D Part layout from the icon menu.</div>
                   </div>
                   <div>
                     <img src={toolSelection} alt="Tool Selection" className="software-screenshot screenshot-medium" style={{ height: '200px' }} />
                   </div>
                 </div>
 
-                <div className={`instruction-step ${currentIndex === 1 ? "reading-active" : ""}`}>
-                  <div className="step-header" style={{ marginTop: "-2.5rem" }}>
-                    <span className="step-number" style={{ padding: "1rem" }}>2</span>
-                    <span className="step-label">Right-click to show the current location of the origin.</span>
-                  </div>
-                </div>
+                <div style={{ marginBottom: "0.8rem" }}>2. Right-click to show the current location of the origin..</div>
+                <div style={{ marginBottom: "0.8rem" }}>3. Left-click on the desired new location point..</div>
+                <div style={{ marginBottom: "0.8rem" }}>4. Left-click on a 2nd point to set the X-axis.</div>
+                <div style={{ marginBottom: "0.8rem" }}>5. Left-click on a 3rd point to set the Y-axis. The XY-plane becomes the front view.</div>
 
-                <div className={`instruction-step ${currentIndex === 2 ? "reading-active" : ""}`}>
-                  <div className="step-header" style={{ marginTop: "-2.5rem" }}>
-                    <span className="step-number" style={{ padding: "0.5rem" }}>3</span>
-                    <span className="step-label">Left-click on the desired new location point.</span>
-                  </div>
-                </div>
 
-                <div className={`instruction-step ${currentIndex === 3 ? "reading-active" : ""}`} style={{ paddingTop: "0.1rem" }}>
-                  <div className="step-header" style={{ marginTop: "-2rem" }}>
-                    <span className="step-number" style={{ padding: "0.5rem" }}>4</span>
-                    <span className="step-label">Left-click on a 2nd point to set the X-axis.</span>
-                  </div>
-                </div>
 
                 <div className={`instruction-step ${currentIndex === 4 ? "reading-active" : ""}`}>
                   <div className="step-header" style={{ marginTop: "-2.5rem" }}>
-                    <span className="step-number" style={{ padding: "0.5rem" }}>5</span>
-                    <span className="step-label">mmmmLeft-click on a 3rd point to set the Y-axis. The XY-plane becomes the front view.</span>
+
+
                   </div>
 
                   <div>
