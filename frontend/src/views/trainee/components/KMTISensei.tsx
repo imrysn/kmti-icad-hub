841866db60032eca3fa101f8c24a0d42bbd7e721
emjiZzz <marygracecastellano0@gmail.com>
2026-07-27 07:27:45 +0800

    fix

--- frontend/src/views/trainee/components/KMTISensei.tsx
+++ frontend/src/views/trainee/components/KMTISensei.tsx
@@ -1,4 +1,4 @@
-import { useCallback,useEffect,useRef } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 import { useTTSContext } from '../../../context/TTSContext'
 
 interface Props {
